25551ea5d60c8107210e5b74b6f5f82d7301ce47
Your Name <you@example.com>
2016-04-01 15:39:37 +0800

    chuangjianwnacheng

---
+++ hehe/hehe/dasfdasfViewController.h
@@ -0,0 +1,13 @@
+//
+//  dasfdasfViewController.h
+//  hehe
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 WHL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface dasfdasfViewController : ViewController
+
+@end
---
+++ hehe/hehe/dasfdasfViewController.m
@@ -0,0 +1,37 @@
+//
+//  dasfdasfViewController.m
+//  hehe
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 WHL. All rights reserved.
+//
+
+#import "dasfdasfViewController.h"
+
+@interface dasfdasfViewController ()
+
+@end
+
+@implementation dasfdasfViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
