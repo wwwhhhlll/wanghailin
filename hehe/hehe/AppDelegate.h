24392b9c02e2361fb9288bf7b12ea79957347d24
Your Name <you@example.com>
2016-04-01 15:36:28 +0800

    第一次创建

---
+++ hehe/hehe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hehe
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 WHL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hehe/hehe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hehe
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 WHL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
